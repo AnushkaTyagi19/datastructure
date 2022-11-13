7bb36d0d1b38a37c9f1c188781c9256e065d7598
AnushkaTyagi19 <118115645+AnushkaTyagi19@users.noreply.github.com>
2022-11-13 19:41:19 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,20 @@
+public class Pattern4 {
+    public static void main(String[] args) {
+        int r=7;
+        int c=7;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i==0||i==r-1||j==0||j==c-1||i==(r/2)&&j==(c/2)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+                System.out.println();
+        }
+
+
+    }
+    
+}
