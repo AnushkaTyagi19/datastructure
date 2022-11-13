2f87d01ea6fcea30791680310bd3e952d53aa029
AnushkaTyagi19 <118115645+AnushkaTyagi19@users.noreply.github.com>
2022-11-13 19:40:27 +0530

    Add files via upload

---
+++ Pattern$.java
@@ -0,0 +1,16 @@
+public class Pattern$ {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int z=0;z<i+1;z++){
+                System.out.print(" ");
+            }
+            for(int j =9-2*i; j>0;j--){
+                System.out.print("*");
+                // k++;
+            }
+            System.out.println();
+        }
+        }
+    }
+    
+
